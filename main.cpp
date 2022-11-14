5ae0c8c5cae036413c6183dedd2a7ae38fbcb8e6
Vincent Prince <vincent.prince.fr@gmail.com>
2022-11-14 17:06:05 +0100

    disable quickedit mode

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <Windows.h>
 
 int main(int argc, char *argv[])
 {
+    // disable QuickEdit mode in Console
+    HANDLE hInput = GetStdHandle(STD_INPUT_HANDLE);
+    DWORD prev_mode;
+    GetConsoleMode(hInput, &prev_mode);
+    SetConsoleMode(hInput, prev_mode & ~ENABLE_QUICK_EDIT_MODE);
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
